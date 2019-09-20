ce82394e1c2c0881a0f46064d00042966d2567bc
安晓东 <xiaodong.an@zhaopin.com.cn>
2019-09-20 14:57:48 +0800

    Fix the issue with the add command

--- utils/index.js
+++ utils/index.js
@@ -1,31 +1,31 @@
-module.exports =  {
+module.exports = {
 
-  randomValue(value,md5){
+  randomValue (value, md5) {
 
-    if(!value || !md5) return 
+    if (!value || !md5) {return}
 
-    const md5Str =  md5(value)
-    const f = String.fromCharCode(Math.floor( Math.random() * 26) + 'a'.charCodeAt())
-    const c = md5Str.slice(-10,md5Str.length)
+    const md5Str = md5(value)
+    const f = String.fromCharCode(Math.floor(Math.random() * 26) + 'a'.charCodeAt())
+    const c = md5Str.slice(-10, md5Str.length)
     return `${f}${c}`
   },
 
-  appndToConfig(options){
-    const {name,value,config,path,flag} = options
-    const obj = {name,value}
-    if( !(config.projects instanceof Array)) {
+  appndToConfig (options) {
+    const {name, value, config, path, flag} = options
+    const obj = {name, value}
+    if (!(config.projects instanceof Array)) {
       config.projects = []
     }
-    if(!config.path){
+    if (!config.path) {
       config.path = config.path || {}
     }
-    const isHadly = config.projects.some(item => item.name === name)
-    if(!isHadly){
+    const isHadly = config.projects.some((item) => item.name === name)
+    if (!isHadly) {
       config.projects.push(obj)
     }
-    
+
     config.path[`${value}${flag}`] = path
-    
+
     return config
   }
 }
